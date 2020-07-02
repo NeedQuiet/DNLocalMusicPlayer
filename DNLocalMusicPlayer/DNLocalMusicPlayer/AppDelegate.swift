3105af2697d945a059ffb2d2e5bdd1a34a1d2aff
Yining Xu <yining.xu@linkplay.com>
2020-07-02 11:10:34 +0800

    初建项目

---
+++ DNLocalMusicPlayer/DNLocalMusicPlayer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DNLocalMusicPlayer
+//
+//  Created by 许一宁 on 2020/7/2.
+//  Copyright © 2020 大宁. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DNLocalMusicPlayer/DNLocalMusicPlayer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DNLocalMusicPlayer
+//
+//  Created by 许一宁 on 2020/7/2.
+//  Copyright © 2020 大宁. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
